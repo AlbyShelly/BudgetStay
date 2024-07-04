ade9b960e28f1c52942ddad63993140c708f1d41
unknown <AlberAD2024>
2024-07-04 13:08:04 +0530

    changr to basil.py

--- algorithms/basil.py
+++ algorithms/basil.py
@@ -1,4 +1,4 @@
-# TODO: aswin
+# TODO: basil
 # Write a function which return the result set according to the user input
 # You may select a machine learning algorithm of your choice
 # To prevent collision, update the choosen algorithm in the project group ASAP
