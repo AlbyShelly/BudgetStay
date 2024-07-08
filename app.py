fd7606e1ae89d07960e53dc4ca105a8b19cebb96
AlbyShelly <albyshelly4@gmail.com>
2024-07-08 06:21:08 +0530

    tested the web app but algorithms still not working

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, request, redirect
-
+from test import get_rows
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    pass
+    
+    if request.method == "GET":
+        return render_template("index.html")
+   
+    
+    else:
+        
+        acNonAc = request.form.get("acNonAc")
+        location = request.form.get("location")
+        cost = request.form.get("cost")
+        cursor = get_rows(location, cost, acNonAc)
+        print(cursor, "cursor")
+        return render_template("result.html", cursor = cursor)
+       
 
 
--- test.py
+++ test.py
@@ -1,50 +1,53 @@
 from pymongo import MongoClient
 
-link = "mongodb+srv://signatureresourcehub:signature@cluster0.ww1qbms.mongodb.net/"
+def get_rows(location, cost, acNonAc):
+    print("location", location, "cost: ", cost, "acNonAc: ", acNonAc)
+    link = "mongodb+srv://signatureresourcehub:signature@cluster0.ww1qbms.mongodb.net/"
+    
+    #connect to mongodb
+    connection = MongoClient(link)
+    print("Connection Complete\n")
+    
+    #choose database
+    db = connection["db_project1"]
+    
+    #choose collection
+    collection = db["hotelrooms"]
+    print(type(collection))
+    
+    #set query parameters
+    #location = "Cochi"
+    #cost = 1000
+    #acNonAc = "NON-AC"
 
-#connect to mongodb
-connection = MongoClient(link)
-print("Connection Complete\n")
-
-#choose database
-db = connection["db_project1"]
-
-#choose collection
-collection = db["hotelrooms"]
-
-#set query parameters
-location = "Cochi"
-cost = 1000
-acNonAc = "NON-AC"
-
-#query
-query = {
-        
-        "$and" : [
-
-            {
-                "location" : {
-                    "$regex" : location
-                }
-        },
-
-            {
-                "cost" : {
-                    "$lte" : cost 
-                }
-        },
-
-            {
-                "acNonAc" : {
-                    "$eq" : "NON-AC"
-                }
-        }
-    ]
-}
-print("Fetching row...") 
-cursor = collection.find(query, sort={ "rating" : -1 } )
-
-for document in cursor:
-    print(document["hotelname"] + " : " +document["location"])
-    print(document["acNonAc"] + " : " + str(document["cost"]) )
-    print()
+    cost = int(cost)
+    
+    #query
+    query = {
+            
+            "$and" : [
+    
+                {
+                    "location" : {
+                        "$regex" : location,
+                        "$options" : "i" 
+                    }
+            },
+    
+                {
+                    "cost" : {
+                        "$lte" : cost 
+                    }
+            },
+    
+                {
+                    "acNonAc" : {
+                        "$eq" : acNonAc
+                    }
+            }
+        ]
+    }
+    print("Fetching row...") 
+    cursor = collection.find(query, sort={ "rating" : -1 } )
+    
+    return cursor                                                                         
